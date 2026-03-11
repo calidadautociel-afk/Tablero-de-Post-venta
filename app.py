6e33d14a6aaa44cb06b96fc3f484fe3b9879dfb7
walterguari <analista.autociel@gmail.com>
2026-03-11 12:45:12 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portal PVT AUTOCIEL", layout="wide")
+
+st.title("Portal de Datos: PVT AUTOCIEL - ANIMACION FOCUS")
+st.write("¡El repositorio y Streamlit están conectados correctamente!")
